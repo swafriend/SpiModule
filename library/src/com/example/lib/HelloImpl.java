036a37f3560235bb8673d8917f843ab7eda9e77a
swafriend <RXR05747@nifty.com>
2023-06-28 20:00:06 +0900

    comment

--- library/src/com/example/lib/HelloImpl.java
+++ library/src/com/example/lib/HelloImpl.java
@@ -3,6 +3,10 @@ package com.example.lib;
 import com.example.app.Hello;
 
 public class HelloImpl implements com.example.app.Hello {
+
+    // library.imp
+    // <orderEntry type="module" module-name="application" />
+    // モジュールの依存関係でapplicationを追加しないと参照できない。
     @Override
     public String hello() {
         return "Hello, SPI!";
